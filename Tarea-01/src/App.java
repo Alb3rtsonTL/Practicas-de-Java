c55b29246e3da7b38e09f41effc2d69d7e1e1e15
Alb3rtsonTL <palb3rtl@gmail.com>
2025-05-21 01:19:26 -0400

    Reorganice y separe

--- Tarea-01/src/App.java
+++ Tarea-01/src/App.java
@@ -3,8 +3,9 @@ import Tarea01.Ejercicios;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Cree un Package para poner cada ejercicio dentro su src correspondiente y después hago la llamada desde el método Main fuera del Package.");
-        System.err.println("Para adelantarme un poco.");
+        System.err.println("Para adelantarme un poco. \n");
+        
         // Llamada al método con el ejercicio
-        Ejercicios.Ejercicio_04();
+        Ejercicios.Ejercicio_10();
     }
 }
