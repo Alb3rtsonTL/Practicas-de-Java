92efd0f39769aa8c77b5d8905da74626369c93e1
Alb3rtsonTL <palb3rtl@gmail.com>
2025-05-23 09:58:31 -0400

    Agregue un comentario de guía

--- Tarea-01/src/Tarea01/example.java
+++
@@ -1,9 +0,0 @@
-package Tarea01;
-
-/** Cree esta clase para ejemplificar si puedo usar la rama develop 
- * para subir archivos y después unirla con la rama main */
-public class example {
-    public static void main(String[] args) {
-        System.out.println("Ejemplo de que no afecta al main");
-    }
-}
