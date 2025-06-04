a4b15d21882bde5fec863bb76fada28665d8b5ae
Alb3rtsonTL <palb3rtl@gmail.com>
2025-06-04 11:02:56 -0400

    Actualiza la clase App para llamar al método principal de la tarea 02 POO y elimina la llamada al ejercicio 22.

--- Tareas-Basicas/src/App.java
+++ Tareas-Basicas/src/App.java
@@ -1,12 +1,13 @@
 import Tarea01_22_Ejercicios.Ejercicios;
+import Tarea02_POO.MainTarea02;
 
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Cree un Package para poner cada practica con sus ejercicios dentro de su src correspondiente y después hago la llamada desde el método Main fuera del Package.");
         System.err.println("Para adelantarme un poco. \n");
         
-        // Llamada al método con el ejercicio
-        // Para ver otro ejercicio pon el numero que le corresponde ejemplo Ejercicios.Ejercicio_NumeroEjercicio()
-        Ejercicios.Ejercicio_22();
+        // Llamada al método principal de la clase para la tarea
+        // Ejercicios.Ejercicio_01(); // Llamada al Ejercicios_01 de la tarea 01
+        MainTarea02.main(); // Llamada al main de la tarea 02 POO
     }
 }
