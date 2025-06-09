9228aeedeba2d44a0f51f44fc633bb5b030223d4
Alb3rtsonTL <palb3rtl@gmail.com>
2025-06-09 19:53:18 -0400

    App.java quite los imports y  comentando el código que no se usa

--- Tareas-Basicas/src/App.java
+++ Tareas-Basicas/src/App.java
@@ -1,5 +1,6 @@
-import Tarea02_POO.MainTarea02;
-import Tarea01_22_Ejercicios.Ejercicios;
+// import Tarea01_22_Ejercicios.Ejercicios; // Comentada porque no se usa
+// import Tarea02_POO.MainTarea02; // Comentada porque no se usa
+import Tarea03_POO_Continuacion.MenuGeneral;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -8,6 +9,7 @@ public class App {
         
         // Llamada al método principal de la clase para la tarea
         // Ejercicios.Ejercicio_01(); // Llamada al Ejercicios_01 de la tarea 01
-        MainTarea02.main(); // Llamada al main de la tarea 02 POO
+        // MainTarea02.main(); // Llamada al main de la tarea 02 POO
+        MenuGeneral.MenuTarea03(); // Llamada al menu general de la tarea 03 POO Continuación
     }
 }
