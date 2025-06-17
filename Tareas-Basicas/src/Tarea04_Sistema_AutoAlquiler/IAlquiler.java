c6a2f34918ae9f96d098f0104ea93057b5b9c9b4
Alb3rtsonTL <palb3rtl@gmail.com>
2025-06-17 18:18:53 -0400

    Añadi la interfaz IAlquiler para definir métodos de alquiler y devolución de vehículos.

---
+++ Tareas-Basicas/src/Tarea04_Sistema_AutoAlquiler/IAlquiler.java
@@ -0,0 +1,7 @@
+package Tarea04_Sistema_AutoAlquiler;
+
+/* Interfaz IAlquiler que define los métodos para alquilar y devolver vehículos. */
+public interface IAlquiler {
+    boolean alquilar();
+    void devolver();
+}
