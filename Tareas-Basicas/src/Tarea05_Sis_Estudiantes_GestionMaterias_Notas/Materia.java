89a0c7e6763a051d40aef1693fd0860b70806c7b
Alb3rtsonTL <palb3rtl@gmail.com>
2025-06-19 16:54:25 -0400

    Actualizar comentarios en la clase abstracta Materia para utilizar la notación Javadoc.

--- Tareas-Basicas/src/Tarea05_Sis_Estudiantes_GestionMaterias_Notas/Materia.java
+++ Tareas-Basicas/src/Tarea05_Sis_Estudiantes_GestionMaterias_Notas/Materia.java
@@ -1,20 +1,22 @@
 package Tarea05_Sis_Estudiantes_GestionMaterias_Notas;
 
-/* Clase abstracta para representar una materia */
+/** Clase abstracta para representar una materia */
 public abstract class Materia {
     private String nombre;
     private int creditos;
 
-    /* Constructor de la clase Materia */
+    /** Constructor de la clase Materia */
     public Materia(String nombre, int creditos) {
         this.nombre = nombre;
         this.creditos = creditos;
     }
 
+    /** Método para obtener el nombre de la materia */
     public String getNombre () { return nombre;}
+    /** Método para obtener los créditos de la materia */
     public int getCreditos() { return creditos;}
 
-    /* Clase abstracta para calcular la calificación como un double */
+    /** Clase abstracta para calcular la calificación como un double */
     public abstract double calcularCalificacion();
 
 }
